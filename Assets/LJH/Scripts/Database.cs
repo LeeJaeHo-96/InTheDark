97563f1c086f0014a446b205f205ec8f0c6b3fcf
LeeJaeHo-96 <towpam123@gmail.com>
2025-02-20 15:47:23 +0900

    Fix : 파이어베이스 연동

---
+++ Assets/LJH/Scripts/Database.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+
+struct ObjectInfo
+{
+    string objName;
+    int objPrice;
+}
+public class Database : MonoBehaviour
+{
+    [Header("저장할 변수")]
+    int money;
+    int days;
+    ObjectInfo obj;
+    
+
+    
+
+}
