45d84deec0bcb24405949bc5994845bc3d4d62f2
LeeJaeHo-96 <towpam123@gmail.com>
2025-02-24 18:00:55 +0900

    Fix : FireBase Load 함수 수정

--- Assets/LJH/Scripts/GameInit.cs
+++ Assets/LJH/Scripts/GameInit.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameInit : BaseUI
 {
@@ -9,6 +10,11 @@ public class GameInit : BaseUI
     [Header("텍스트")]
     public TMP_Text dayText;
     public TMP_Text moneyText;
+
+    [Header("버튼")]
+    public Button exitButton;
+    public Button PlusDayButton;
+    public Button PlusMoneyButton;
 
     void Awake()
     {
@@ -22,9 +28,40 @@ public class GameInit : BaseUI
         Database.instance.moneyText = moneyText;
     }
 
+    void ExitButton()
+    {
+#if UNITY_EDITOR
+        //Comment : 유니티 에디터상에서 종료
+        Database.instance.SaveData(FirebaseManager.Auth.CurrentUser.UserId, "Slot_1", int.Parse(moneyText.text), int.Parse(dayText.text));
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        //Comment : 빌드 상에서 종료
+        Application.Quit();
+#endif
+    }
+
+    void PlusDay()
+    {
+            Database.instance.days += 1;
+            dayText.text = Database.instance.days.ToString();
+    }
+    void PlusMoney()
+    {
+            Database.instance.money += 100;
+            moneyText.text = Database.instance.money.ToString();
+    }
     void Init()
     {
         dayText = GetUI<TMP_Text>("Day");
         moneyText = GetUI<TMP_Text>("Money");
+
+        exitButton = GetUI<Button>("ExitButton");
+        exitButton.onClick.AddListener(ExitButton);
+
+        PlusDayButton = GetUI<Button>("PlusDay");
+        PlusMoneyButton = GetUI<Button>("PlusMoney");
+
+        PlusDayButton.onClick.AddListener(PlusDay);
+        PlusMoneyButton.onClick.AddListener(PlusMoney);
     }
 }
