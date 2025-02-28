064d709db6d3343a8828b262d7b13443f5af0ec7
Vivid <suook15@naver.com>
2025-02-28 19:03:48 +0900

    [Feat] 방 코드 필터링 기능 추가

--- Assets/BSM/Scripts/Enum.cs
+++ Assets/BSM/Scripts/Enum.cs
@@ -7,3 +7,9 @@ public enum Settings
 {
     FRAME, WINDOWMODE, GAMMA, MASTER_VOLUME, BGM_VOLUME, SFX_VOLUME, SIZE
 }
+
+[System.Serializable]
+public enum RoomState
+{
+    PUBLIC, FRIENDS_ONLY
+}
--- Assets/BSM/Scripts/Lobby/Lobby.cs
+++ Assets/BSM/Scripts/Lobby/Lobby.cs
@@ -4,19 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Lobby : ObjBind
 {
-
-    private RectTransform _roomContentRect;
-    private RoomJoin _roomJoinPrefab;
     private Dictionary<string, RoomJoin> _roomJoinDict = new Dictionary<string, RoomJoin>();
+    private Dictionary<string, string> _roomCodeDict = new Dictionary<string, string>();
+    
+    private List<RoomInfo> _roomList = new List<RoomInfo>();
+    private RoomJoin _roomJoinPrefab;
 
+    private TMP_InputField _roomCode;
+    private RectTransform _roomContentRect; 
     private Button _refreshButton;
     private Button _leftLobbyButton;
-        
+    private string _roomCodeStr = "";    
+    
     private void Awake() => Init();
  
     private void Init()
@@ -26,7 +31,7 @@ public class Lobby : ObjBind
         _roomContentRect = GetComponentBind<RectTransform>("Room_Content");
         _refreshButton = GetComponentBind<Button>("Refresh_Button");
         _leftLobbyButton = GetComponentBind<Button>("Back_Button");
-        
+        _roomCode = GetComponentBind<TMP_InputField>("Room_Code_Field");
         OnClickListener();
         
         _roomJoinPrefab = Resources.Load<RoomJoin>("Room_Prefabs");
@@ -37,17 +42,49 @@ public class Lobby : ObjBind
         _leftLobbyButton.onClick.AddListener(() => PhotonNetwork.LeaveLobby()); 
         _refreshButton.onClick.AddListener(() =>
         {
-            PhotonNetwork.LeaveRoom();
+            PhotonNetwork.LeaveLobby();
             PhotonNetwork.JoinLobby(); 
         });
+        
+        _roomCode.onValueChanged.AddListener(x => RoomCodeCompare(x)); 
     }
 
+    private void RoomCodeCompare(string x)
+    {
+        foreach (RoomInfo room in _roomList)
+        {
+
+            if (!_roomCodeDict.ContainsKey(room.CustomProperties["RoomCode"].ToString()))
+            {
+                _roomCodeStr = room.CustomProperties["RoomCode"].ToString();
+            }
+            
+            if (x.Length > 0)
+            {
+                if (!_roomCodeStr.Equals(x))
+                {
+                    _roomJoinDict[room.Name].gameObject.SetActive(false);
+                }
+                else
+                {
+                    _roomJoinDict[room.Name].gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                _roomJoinDict[room.Name].gameObject.SetActive(true);
+            }
+        }
+    }
+    
     /// <summary>
     /// 로비 입장 시 방 정보 갱신
     /// </summary>
     /// <param name="roomList"></param>
     public void RoomListUpdate(List<RoomInfo> roomList)
     {
+        _roomList = roomList;
+        
         foreach (RoomInfo info in roomList)
         {
             if (info.RemovedFromList || !info.IsVisible || !info.IsOpen)
@@ -73,6 +110,9 @@ public class Lobby : ObjBind
         
     }
 
+    /// <summary>
+    /// 기존 방 리스트 삭제
+    /// </summary>
     public void ClearRoom()
     {
         foreach (string name in _roomJoinDict.Keys)
