61f872efbfff285be3e38104793f6c488112ad99
LeeJaeHo-96 <towpam123@gmail.com>
2025-03-14 11:27:00 +0900

    Fix : 맵 생성 로직 네트워크로 수정

--- Assets/LJH/MapScene/Scripts/Map/MapDoorOnOff.cs
+++ Assets/LJH/MapScene/Scripts/Map/MapDoorOnOff.cs
@@ -1,40 +1,72 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapDoorOnOff : MonoBehaviour
+public class MapDoorOnOff : MonoBehaviourPun
 {
     [SerializeField] List<GameObject> lockDoorList;
     [SerializeField] List<GameObject> openDoorList;
 
     private void Start()
     {
-
-        if(openDoorList.Count > 0)
-            CreateDoor(lockDoorList, openDoorList);
-        else
-            CreateDoor(lockDoorList);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            if (openDoorList.Count > 0)
+                CreateDoor(lockDoorList, openDoorList);
+            else
+                CreateDoor(lockDoorList);
+        }
     }
 
-    static void CreateDoor(List<GameObject> lockList, List<GameObject> openList)
+    void CreateDoor(List<GameObject> lockList, List<GameObject> openList)
     {
+        bool[] lockStates = new bool[lockList.Count];
+
         //문 한쪽만 있게 테스트용
         //for (int i = 0; i < lockList.Count; i++)
         for (int i = 0; i < 2; i++)
         {
             if (!lockList[i].activeSelf)
-                lockList[i].SetActive(Random.Range(0, 3) < 0.5f);
-
+            {
+                lockStates[i] = (Random.Range(0, 3) < 0.5f);
+                lockList[i].SetActive(lockStates[i]);
+            }
             openList[i].SetActive(!lockList[i].activeSelf);
         }
+
+        photonView.RPC("RPCSyncDoors", RpcTarget.Others, lockStates);
     }
 
-    static void CreateDoor(List<GameObject> lockList)
+    void CreateDoor(List<GameObject> lockList)
     {
-        for (int i = 0; i < lockList.Count; i++)
+        bool[] lockStates = new bool[lockList.Count];
+
+        //문 한쪽만 있게 테스트용
+        //for (int i = 0; i < lockList.Count; i++)
+        for (int i = 0; i < 2; i++)
         {
             if (!lockList[i].activeSelf)
-                lockList[i].SetActive(Random.Range(0, 3) < 0.5f);
+            {
+                lockStates[i] = (Random.Range(0, 3) < 0.5f);
+                lockList[i].SetActive(lockStates[i]);
+            }
+        }
+
+        photonView.RPC("RPCSyncDoors", RpcTarget.Others, lockStates);
+    }
+
+    [PunRPC]
+    void RPCSyncDoors(bool[] lockStates)
+    {
+        for (int i = 0; i < lockStates.Length; i++)
+        {
+            lockDoorList[i].SetActive(lockStates[i]);
+
+            if(openDoorList.Count > i)
+            {
+                openDoorList[i].SetActive(!lockStates[i]);
+            }
         }
     }
 }
