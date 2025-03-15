7abd75c4cba2a7b4b9cb192f059690345beb1e6a
LeeJaeHo-96 <towpam123@gmail.com>
2025-03-15 19:07:39 +0900

    Fix : 몬스터 리지드바디 추가 및  오브젝트 위치 수정

--- Assets/LJH/MapScene/Scripts/SpawPos.cs
+++ Assets/LJH/MapScene/Scripts/SpawPos.cs
@@ -1,14 +1,21 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawPos : MonoBehaviour
+public class SpawPos : MonoBehaviourPun
 {
     GameObject player;
     void Start()
     {
         player = GameObject.FindWithTag(Tag.Player);
 
+        photonView.RPC("RPCplayerPosSet", RpcTarget.AllViaServer);
+    }
+
+    [PunRPC]
+    void RPCplayerPosSet()
+    {
         player.transform.position = transform.position;
     }
 
