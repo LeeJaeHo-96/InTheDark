09899138c064d84b32302f7701c2d48a56ba0798
Vivid <suook15@naver.com>
2025-03-21 16:34:44 +0900

    [Feat] 방 이탈 시 커서 락 해제

--- Assets/BSM/Scripts/Network/PunManager.cs
+++ Assets/BSM/Scripts/Network/PunManager.cs
@@ -55,6 +55,7 @@ public class PunManager : MonoBehaviourPunCallbacks
 
     public override void OnLeftRoom()
     {
+        Cursor.lockState = CursorLockMode.None;
         GoToStartScene();
     }
 
